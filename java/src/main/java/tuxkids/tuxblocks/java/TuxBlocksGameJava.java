2956cce261e036993023ae942dfebb627fc62c2e
Thomas Price <thomaswprice@msn.com>
2013-08-26 18:51:30 -0400

    Java Icon

--- java/src/main/java/tuxkids/tuxblocks/java/TuxBlocksGameJava.java
+++ java/src/main/java/tuxkids/tuxblocks/java/TuxBlocksGameJava.java
@@ -1,11 +1,20 @@
 package tuxkids.tuxblocks.java;
 
 import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.IntBuffer;
 
 import javax.imageio.ImageIO;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.Display;
 
 import playn.core.CanvasImage;
 import playn.core.PlayN;
@@ -40,8 +49,53 @@ public class TuxBlocksGameJava {
 		config.storageFileName = "TuxBlocks.dat";
 		// use config to customize the Java platform, if needed
 		JavaPlatform platform = JavaPlatform.register(config);
-		// TODO: Display.setIcon()
+		// load the icon.. laboriously
+		try {
+			URL url16 = TuxBlocksGame.class.getClassLoader().getResource("assets/images/icon-16.png");
+			URL url32 = TuxBlocksGame.class.getClassLoader().getResource("assets/images/icon-32.png");
+			BufferedImage icon16 = ImageIO.read(url16);
+			BufferedImage icon32 = ImageIO.read(url32);
+
+			Display.setIcon(new ByteBuffer[] { createBuffer2(icon16), createBuffer2(icon32) });
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		platform.setTitle("TuxBlocks");
 		PlayN.run(new TuxBlocksGame());
+	}
+
+	// modified from: http://www.jpct.net/forum2/index.php?topic=795.0
+	private static ByteBuffer createBuffer2(BufferedImage img) {
+		int len=img.getHeight(null)*img.getWidth(null);
+		ByteBuffer temp=ByteBuffer.allocateDirect(len<<2);;
+		temp.order(ByteOrder.LITTLE_ENDIAN);
+
+		int[] pixels=new int[len];
+
+		PixelGrabber pg = new PixelGrabber(img, 0, 0, 
+				img.getWidth(null), img.getHeight(null), 
+				pixels, 0, img.getWidth(null));
+
+		try {
+			pg.grabPixels();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		for (int i=0; i<len; i++) {
+			int pos=i<<2;
+			int texel=pixels[i];
+			if (texel != 0) {
+				System.out.println("!");
+			}
+			int a = (texel >> 24) & 0xff;
+			int r = (texel >> 16) & 0xff;
+			int g = (texel >> 8) & 0xff;
+			int b = (texel >> 0) & 0xff;
+			int color = a << 24 | b << 16 | g << 8 | r;
+			temp.putInt(pos, color);
+		}
+
+		return temp;
 	}
 }
