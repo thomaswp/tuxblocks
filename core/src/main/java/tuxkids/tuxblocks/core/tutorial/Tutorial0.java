3e860981bc3cd09d074222a04f9b1fb494ff4095
Thomas Price <thomaswprice@msn.com>
2014-03-25 23:36:48 -0400

    Working version of FSM tutorial

--- core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial0.java
+++ core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial0.java
@@ -1,9 +1,24 @@
 package tuxkids.tuxblocks.core.tutorial;
 
+import tuxkids.tuxblocks.core.tutorial.Tutorial.Trigger;
+
 public class Tutorial0 extends FSMTutorial {
 
 	public Tutorial0(int themeColor) {
 		super(themeColor);
+	}
+
+	@Override
+	protected void addStates() {
+		State one = addStartState("firstMessage");
+		State two = addState("secondMessage");
+		State three = addState("thirdMessage");
+		
+		one.addTransition(Trigger.TextBoxHidden, two);
+		two.addTransition(Trigger.Build_Shown, three);
+		three.addTransition(Trigger.Title_Build, endState);
+		
+		anyState.addTransition(Trigger.Title_Play, two);
 	}
 
 
