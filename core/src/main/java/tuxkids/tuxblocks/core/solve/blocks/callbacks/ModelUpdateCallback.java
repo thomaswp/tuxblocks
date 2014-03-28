684bbe1e5fb80ad23e046410a6059149d8aab3ad
Thomas Price <thomaswprice@msn.com>
2014-03-28 11:05:11 -0400

    Added limits to A*

---
+++ core/src/main/java/tuxkids/tuxblocks/core/solve/blocks/callbacks/ModelUpdateCallback.java
@@ -0,0 +1,14 @@
+package tuxkids.tuxblocks.core.solve.blocks.callbacks;
+
+import tuxkids.tuxblocks.core.solve.action.SolveAction;
+import tuxkids.tuxblocks.core.solve.blocks.Equation;
+import tuxkids.tuxblocks.core.solve.blocks.EquationManipulator.SolveActionCallback;
+
+public class ModelUpdateCallback implements SolveActionCallback {
+
+	@Override
+	public void onActionPerformed(SolveAction action, Equation before) {
+		
+	}
+
+}
