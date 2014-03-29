a0b28da7d160e4a5010faf1fc65b916d65af7767
Kevin Lubick <kjlubick@ncsu.edu>
2014-03-29 11:45:23 -0400

    Stopped bugging the user when they correct a mistaken drag

--- core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial1.java
+++ core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial1.java
@@ -4,11 +4,14 @@ package tuxkids.tuxblocks.core.tutorial;
 import static tuxkids.tuxblocks.core.tutorial.Tutorial.Tag.*;
 import static tuxkids.tuxblocks.core.tutorial.Tutorial.Trigger.*;
 import tuxkids.tuxblocks.core.solve.blocks.Equation;
+import tuxkids.tuxblocks.core.solve.blocks.EquationBlockIndex;
 import tuxkids.tuxblocks.core.story.StoryGameState;
 import tuxkids.tuxblocks.core.utils.Debug;
 
 
 public class Tutorial1 extends FSMTutorial {
+
+	protected boolean hasCoachedOnExtraDragging = false;
 
 	public Tutorial1(StoryGameState storyGameState) {
 		super(storyGameState);
@@ -63,7 +66,7 @@ public class Tutorial1 extends FSMTutorial {
 
 	private FSMState solve3x4asFirstEquation(final FSMState _3x4One) {
 		FSMState _3x4Two = addState("id_solveForX");
-		FSMState _3x4Three = addState("id_3x4simplifyMultiplicationPrompt");
+		final FSMState _3x4Three = addState("id_3x4simplifyMultiplicationPrompt");
 		FSMState _3x4Simplify1 = addState("id_3x4simplifyScreen1");
 		FSMState _3x4Simplify2 = addState("id_simplifyScreen2");
 		FSMState _3x4Simplify3 = addState("id_simplifyScreen3");
@@ -71,12 +74,35 @@ public class Tutorial1 extends FSMTutorial {
 		FSMState _3x4Simplify5 = addState("id_simplifyScreen5").addHighlightable(NumberSelect_Ok);
 		FSMState _3x4Solved = addState("id_3x4solved").addHighlightable(Solve_Ok);
 		
-		FSMState _3x4dragged3One = addState("id_didNotClickGreyCircle");
+		
+		
+		final FSMState _3x4dragged3One = addState("id_didNotClickGreyCircle", new FSMState(){
+			@Override
+			public FSMState notifyMessageShown() {
+				hasCoachedOnExtraDragging  = true;
+				return super.notifyMessageShown();
+			}
+		});
 		FSMState _3x4dragged3Two = addState("id_3x4dragged3");
+		final FSMState _3x4FixedBadDrag = addState("id_3x4fixDrag");
+		_3x4FixedBadDrag.registerEpsilonTransition(_3x4Three);
+		
 		
 		joinMultiTextStates(_3x4One, _3x4Two, _3x4Three);
 		_3x4Three.addTransition(_3x4Simplify1, NumberSelect_Shown);
-		_3x4Three.addTransition(_3x4dragged3One, Solve_BlockReleasedOnOther);
+		_3x4Three.addTransition(new StateChooser() {
+			
+			@Override
+			public FSMState chooseState(Object extraInformation) {
+				if (extraInformation instanceof EquationBlockIndex) {
+					if (((EquationBlockIndex) extraInformation).expressionIndex == 0) 
+						return _3x4dragged3One;
+					return _3x4FixedBadDrag;
+				}
+				Debug.write("no drag information?");
+				return _3x4FixedBadDrag;
+			}
+		}, Solve_BlockReleasedOnOther);
 		
 		joinMultiTextStates(_3x4Simplify1, _3x4Simplify2, _3x4Simplify3, _3x4Simplify4);
 		_3x4Simplify4.addTransition(_3x4Simplify5, NumberSelect_NumberSelected);
@@ -88,19 +114,35 @@ public class Tutorial1 extends FSMTutorial {
 		return _3x4Solved;
 	}
 
-	private FSMState solve3x4asSecondEquation(FSMState _6m5SolvedFirst) {
-		// TODO Auto-generated method stub
-		return null;
+	private FSMState solve6m5asSecondEquation(FSMState _3x4SolvedFirst) {
+		FSMState _6m5One = addState("id_unlockOther");
+		FSMState _6m5Two = addState("id_6-5secondSubtract");
+		FSMState _6m5Simplify = addState("id_secondSimplify");
+		FSMState _6m5Solved = addState("id_secondSolved");
+
+		_3x4SolvedFirst.addTransition(_6m5One, Select_ScreenShown);
+		_6m5One.addTransition(_6m5Two, Solve_ScreenShown);
+		
+		_6m5Two.addTransition(new StateChooser() {
+			
+			@Override
+			public FSMState chooseState(Object extraInformation) {
+				// TODO Auto-generated method stub
+				return null;
+			}
+		}, Solve_BlockReleasedOnOther);
+		
+		return _6m5Solved;
 	}
 
 	private FSMState solve6m5asFirstEquation(FSMState _6m5One) {
 		// TODO Auto-generated method stub
-		return null;
+		return new FSMState();
 	}
 
-	private FSMState solve6m5asSecondEquation(FSMState _3x4SolvedFirst) {
+	private FSMState solve3x4asSecondEquation(FSMState _6m5SolvedFirst) {
 		// TODO Auto-generated method stub
-		return null;
+		return new FSMState();
 	}
 
 	@Override
