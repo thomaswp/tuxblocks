dd35763f979f9be5b53bb9c4643e0f2ca393ea18
Kevin Lubick <kjlubick@ncsu.edu>
2014-03-29 13:40:46 -0400

    Added in case for premature simplification
    If the user types an answer, Tux will skip to his last piece about
    entering the answer.

--- core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial1.java
+++ core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial1.java
@@ -19,17 +19,30 @@ public class Tutorial1 extends FSMTutorial {
 
 	@Override
 	protected void setUpStates() {
-		FSMState one = addStartState("id_salvageAndBuild");
+		FSMState start = addStartState("id_salvageAndBuild");
+		final FSMState _3x4First = addState("id_equationSolvingScreen");
+		final FSMState _6m5First = addState("id_equationSolvingScreen");	//6m5 means 6 minus 5 (6-5)
+		FSMState letsGoSetupRobots = addState("id_letsGoSetupRobots").addHighlightable(Select_Return);
+		
+		this.promptSelectionOfFirstEquation(start, _3x4First, _6m5First);
+		
+		FSMState _3x4SolvedFirst = this.solve3x4asFirstEquation(_3x4First);
+		FSMState _6m5SolvedSecond = this.solve6m5asSecondEquation(_3x4SolvedFirst);
+
+		FSMState _6m5SolvedFirst = this.solve6m5asFirstEquation(_6m5First);
+		FSMState _3x4SolvedSecond = this.solve3x4asSecondEquation(_6m5SolvedFirst);
+		
+		_6m5SolvedSecond.addTransition(letsGoSetupRobots, Select_ScreenShown);
+		_3x4SolvedSecond.addTransition(letsGoSetupRobots, Select_ScreenShown);
+		
+		letsGoSetupRobots.addTransition(endState, Defense_ScreenShown);
+	}
+
+	private void promptSelectionOfFirstEquation(FSMState one, final FSMState _3x4First, final FSMState _6m5First) {
 		FSMState two = addState("id_goToEquationScreen").addHighlightable(Defense_EquationSelectScreen);
 		FSMState three = addState("id_solveTheClues");
 		final FSMState four = addState("id_selectFirstEquation").addHighlightable(Select_FirstEquation).
-				addHighlightable(Select_SecondEquation);
-		
-		final FSMState _3x4One = addState("id_equationSolvingScreen");
-		//6m5 means 6 minus 5 (6-5)
-		final FSMState _6m5One = addState("id_equationSolvingScreen");
-		
-		FSMState letsGoSetupRobots = addState("id_letsGoSetupRobots").addHighlightable(Select_Return);
+		addHighlightable(Select_SecondEquation);
 		
 		one.addTransition(two, TextBoxHidden);
 		two.addTransition(three, Select_ScreenShown);
@@ -41,9 +54,9 @@ public class Tutorial1 extends FSMTutorial {
 				if (extraInformation instanceof Equation) {
 					String eqName = ((Equation) extraInformation).name();
 					if ("3x4".equals(eqName)) {
-						return _3x4One;
+						return _3x4First;
 					} else if ("6-5".equals(eqName)) {
-						return _6m5One;
+						return _6m5First;
 					} 
 					Debug.write("Unknown equation named "+eqName+" "+extraInformation);
 					return endState;
@@ -52,16 +65,6 @@ public class Tutorial1 extends FSMTutorial {
 				return four;
 			}
 		}, Solve_ScreenShown);
-		
-		FSMState _3x4SolvedFirst = solve3x4asFirstEquation(_3x4One);
-		FSMState _6m5SolvedSecond = solve6m5asSecondEquation(_3x4SolvedFirst);
-
-		FSMState _6m5SolvedFirst = solve6m5asFirstEquation(_6m5One);
-		FSMState _3x4SolvedSecond = solve3x4asSecondEquation(_6m5SolvedFirst);
-		
-		_6m5SolvedSecond.addTransition(letsGoSetupRobots, Select_ScreenShown);
-		_3x4SolvedSecond.addTransition(letsGoSetupRobots, Select_ScreenShown);
-		letsGoSetupRobots.addTransition(endState, Defense_ScreenShown);
 	}
 
 	private FSMState solve3x4asFirstEquation(final FSMState _3x4One) {
@@ -72,11 +75,31 @@ public class Tutorial1 extends FSMTutorial {
 		FSMState _3x4Simplify3 = addState("id_simplifyScreen3");
 		FSMState _3x4Simplify4 = addState("id_simplifyScreen4");
 		FSMState _3x4Simplify5 = addState("id_simplifyScreen5").addHighlightable(NumberSelect_Ok);
+		FSMState _3x4PrematureSimplify = addState("id_prematureSimplify");	
 		FSMState _3x4Solved = addState("id_3x4solved").addHighlightable(Solve_Ok);
+
+		joinMultiTextStates(_3x4One, _3x4Two, _3x4Three);
+		solve3x4AsFirst_handleDrag(_3x4Three);
 		
+		_3x4Three.addTransition(_3x4Simplify1, NumberSelect_Shown);
+		joinMultiTextStates(_3x4Simplify1, _3x4Simplify2, _3x4Simplify3, _3x4Simplify4);
+		handlePrematureSimplification(_3x4PrematureSimplify, _3x4Simplify5, _3x4Simplify1, _3x4Simplify2, _3x4Simplify3);
+		_3x4Simplify4.addTransition(_3x4Simplify5, NumberSelect_NumberSelected);
 		
-		
-		final FSMState _3x4dragged3One = addState("id_didNotClickGreyCircle", new FSMState(){
+
+		_3x4Simplify5.addTransition(_3x4Solved, Solve_Solved);
+		return _3x4Solved;
+	}
+
+	private void handlePrematureSimplification(FSMState simplifyNode, FSMState nodeToSkipTo,FSMState... skippableNodes) {
+		for(FSMState node:skippableNodes) {
+			node.addTransition(simplifyNode, NumberSelect_NumberSelected);
+		}
+		joinMultiTextStates(simplifyNode, nodeToSkipTo);	
+	}
+
+	private void solve3x4AsFirst_handleDrag(final FSMState startingNode) {
+		final FSMState _3x4draggedOne = addState("id_didNotClickGreyCircle", new FSMState(){
 			@Override
 			public FSMState notifyMessageShown() {
 				hasCoachedOnExtraDragging  = true;
@@ -85,54 +108,70 @@ public class Tutorial1 extends FSMTutorial {
 		});
 		FSMState _3x4dragged3Two = addState("id_3x4dragged3");
 		final FSMState _3x4FixedBadDrag = addState("id_3x4fixDrag");
-		_3x4FixedBadDrag.registerEpsilonTransition(_3x4Three);
-		
-		
-		joinMultiTextStates(_3x4One, _3x4Two, _3x4Three);
-		_3x4Three.addTransition(_3x4Simplify1, NumberSelect_Shown);
-		_3x4Three.addTransition(new StateChooser() {
 			
+		startingNode.addTransition(new StateChooser() {	
 			@Override
 			public FSMState chooseState(Object extraInformation) {
 				if (extraInformation instanceof EquationBlockIndex) {
 					if (((EquationBlockIndex) extraInformation).expressionIndex == 0) 
-						return _3x4dragged3One;
+						return _3x4draggedOne;
 					return _3x4FixedBadDrag;
 				}
 				Debug.write("no drag information?");
 				return _3x4FixedBadDrag;
 			}
-		}, Solve_BlockReleasedOnOther);
-		
-		joinMultiTextStates(_3x4Simplify1, _3x4Simplify2, _3x4Simplify3, _3x4Simplify4);
-		_3x4Simplify4.addTransition(_3x4Simplify5, NumberSelect_NumberSelected);
-		
-		joinMultiTextStates(_3x4dragged3One, _3x4dragged3Two);
-		_3x4dragged3Two.registerEpsilonTransition(_3x4Three);
-		
-		_3x4Simplify5.addTransition(_3x4Solved, Solve_Solved);
-		return _3x4Solved;
+		}, Solve_BlockReleasedOnOther);		
+		joinMultiTextStates(_3x4draggedOne, _3x4dragged3Two);
+		_3x4dragged3Two.registerEpsilonTransition(startingNode);
+		_3x4FixedBadDrag.registerEpsilonTransition(startingNode);
 	}
 
 	private FSMState solve6m5asSecondEquation(FSMState _3x4SolvedFirst) {
 		FSMState _6m5One = addState("id_unlockOther");
-		FSMState _6m5Two = addState("id_6-5secondSubtract");
+		FSMState _6m5Two = addState("id_6m5secondSubtract");
 		FSMState _6m5Simplify = addState("id_secondSimplify");
-		FSMState _6m5Solved = addState("id_secondSolved");
+		FSMState _6m5Solved = addState("id_secondSolved").addHighlightable(Solve_Ok);
 
 		_3x4SolvedFirst.addTransition(_6m5One, Select_ScreenShown);
 		_6m5One.addTransition(_6m5Two, Solve_ScreenShown);
+		
+		solve6m5AsSecond_handleDrag(_6m5Two);
+		
+		_6m5Two.addTransition(_6m5Simplify, NumberSelect_Shown);
+		_6m5Simplify.addTransition(_6m5Solved, Solve_Solved);
+		
+		return _6m5Solved;
+	}
+
+	private void solve6m5AsSecond_handleDrag(FSMState _6m5Two) {
+		final FSMState _6m5Dragged = addState("id_didNotClickGreyCircle");
+		final FSMState _6m5DraggedTwo = addState("id_6m5secondDragged5");
+		final FSMState _6m5DraggedReminder = addState("id_6m5secondDragged5reminder");
+		final FSMState _6m5FixedBadDrag = addState("id_6m5fixDrag");
 		
 		_6m5Two.addTransition(new StateChooser() {
 			
 			@Override
 			public FSMState chooseState(Object extraInformation) {
-				// TODO Auto-generated method stub
-				return null;
+				if (extraInformation instanceof EquationBlockIndex) {
+					if (((EquationBlockIndex) extraInformation).expressionIndex == 0) {
+						if (hasCoachedOnExtraDragging) {
+							return _6m5DraggedReminder;
+						} 
+						return _6m5Dragged;
+					}
+					return _6m5FixedBadDrag;
+				}
+				Debug.write("no drag information?");
+				return _6m5FixedBadDrag;
 			}
 		}, Solve_BlockReleasedOnOther);
 		
-		return _6m5Solved;
+		joinMultiTextStates(_6m5Dragged, _6m5DraggedTwo);
+		
+		_6m5DraggedReminder.registerEpsilonTransition(_6m5Two);
+		_6m5DraggedTwo.registerEpsilonTransition(_6m5Two);
+		_6m5FixedBadDrag.registerEpsilonTransition(_6m5Two);
 	}
 
 	private FSMState solve6m5asFirstEquation(FSMState _6m5One) {
