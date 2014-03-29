7a7e26a560df20fb3cf65708a8c6f8c87cba953a
Thomas Price <thomaswprice@msn.com>
2014-03-29 11:20:42 -0400

    Modified tutorials to used *_Base interfaces

--- core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial1.java
+++ core/src/main/java/tuxkids/tuxblocks/core/tutorial/Tutorial1.java
@@ -4,10 +4,11 @@ package tuxkids.tuxblocks.core.tutorial;
 import static tuxkids.tuxblocks.core.tutorial.Tutorial.Tag.Defense_EquationSelectScreen;
 import static tuxkids.tuxblocks.core.tutorial.Tutorial.Trigger.*;
 import tuxkids.tuxblocks.core.story.StoryGameState;
+import tuxkids.tuxblocks.core.tutorial.gen.Tutorial1_1_Base;
 import tuxkids.tuxblocks.core.utils.Debug;
 
 
-public class Tutorial1 extends FSMTutorial {
+public class Tutorial1 extends FSMTutorial implements Tutorial1_1_Base {
 
 	public Tutorial1(StoryGameState storyGameState) {
 		super(storyGameState);
@@ -15,10 +16,10 @@ public class Tutorial1 extends FSMTutorial {
 
 	@Override
 	protected void setUpStates() {
-		FSMState one = addStartState("id_salvageAndBuild");
-		FSMState two = addState("id_goToEquationScreen").addHighlightable(Defense_EquationSelectScreen);
-		FSMState three = addState("id_solveTheClues");
-		FSMState four = addState("id_selectFirstEquation");
+		FSMState one = addStartState(id_salvageAndBuild);
+		FSMState two = addState(id_goToEquationScreen).addHighlightable(Defense_EquationSelectScreen);
+		FSMState three = addState(id_solveTheClues);
+		FSMState four = addState(id_selectFirstEquation);
 		
 		one.addTransition(two, TextBoxHidden);
 		two.addTransition(three, Select_Shown);
